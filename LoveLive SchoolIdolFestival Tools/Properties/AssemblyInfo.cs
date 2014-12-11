bb74ecc71b59f4702687245e878d547429c146ec
Kouki.H <syskenhamakou@gmail.com>
2014-12-11 12:55:21 +0900

    Ver 1.2.4.1
    見えない部分の更新

--- LoveLive SchoolIdolFestival Tools/Properties/AssemblyInfo.cs
+++ LoveLive SchoolIdolFestival Tools/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ノート重要度計算機")]
+[assembly: AssemblyTitle("LoveLive SchoolIdolFestival Tools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ノート重要度計算機")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("LoveLive SchoolIdolFestival Tools")]
+[assembly: AssemblyCopyright("Copyright ©metasequo  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
